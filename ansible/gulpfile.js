3ea708a8698d43242cb15913544067e2a009d660
joeymadich <joe@devcorr.com>
2014-03-19 17:49:55 -0400

    starting ansible playbook to pull in gulp plugins to use in defining gulp tasks for deploying devcorr.com to github pages

---
+++ ansible/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+
+//plugins
+var imagemin = require('gulp-imagemin');	
+var sass = require('gulp-sass');
+var git = require('gulp-git');
+var uglify = require('gulp-closure-compiler');
+
+gulp.task('default', function() {
+	console.log('the following tasks are used to deploy devcorr.com');
+});
