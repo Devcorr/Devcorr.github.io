d05e1f7070ea366d66e3a1b8ec479d5567246b6f
joeymadich <joe@devcorr.com>
2014-03-20 17:29:00 -0400

    updated gulpfile for JS compression, sass compilation and image optimization

--- ansible/gulpfile.js
+++ ansible/gulpfile.js
@@ -4,8 +4,33 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');	
 var sass = require('gulp-sass');
 var git = require('gulp-git');
-var uglify = require('gulp-closure-compiler');
+var closureCompiler = require('gulp-closure-compiler');
 
-gulp.task('default', function() {
-	console.log('the following tasks are used to deploy devcorr.com');
+var projectRoot = '../www/';
+var paths = {
+	scripts: projectRoot + 'js/**/*',
+	scss: projectRoot + 'scss/',
+	css: projectRoot + 'css/**/*',
+	fonts: projectRoot + 'fonts/**/*',
+	images: projectRoot + 'images/'
+};
+
+gulp.task('scripts', function() {
+	return gulp.src(paths.scripts)
+		.pipe(closureCompiler({language_in: 'ECMASCRIPT5_STRICT'}))
+		.pipe(gulp.dest(projectRoot+'js'));
 });
+
+gulp.task('images', function() {
+	return gulp.src(paths.images + '*.png')
+		.pipe(imagemin({optimizationLevel: 5}))
+		.pipe(gulp.dest(projectRoot+'images'));
+});
+
+gulp.task('sass', function() {
+	return gulp.src(paths.scss + '*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest(projectRoot+'css'));
+});
+
+gulp.task('default', ['scripts','images','sass']);
