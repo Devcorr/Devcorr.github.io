6f6c78772f798c38a7998c1b66eaffc3e8cb3031
joeymadich <joe@devcorr.com>
2014-03-21 15:44:27 -0400

    updating gulpfile and starting to add in git tasks

--- ansible/gulpfile.js
+++ ansible/gulpfile.js
@@ -3,34 +3,45 @@ var gulp = require('gulp');
 //plugins
 var imagemin = require('gulp-imagemin');	
 var sass = require('gulp-sass');
-var git = require('gulp-git');
+var shell = require('gulp-shell');
 var closureCompiler = require('gulp-closure-compiler');
+var minifyCSS = require('gulp-minify-css');
 
 var projectRoot = '../www/';
 var paths = {
 	scripts: projectRoot + 'js/**/*',
 	scss: projectRoot + 'scss/',
-	css: projectRoot + 'css/**/*',
-	fonts: projectRoot + 'fonts/**/*',
+	css: projectRoot + 'css/',
+	fonts: projectRoot + 'fonts/',
 	images: projectRoot + 'images/'
 };
 
-gulp.task('scripts', function() {
-	return gulp.src(paths.scripts)
+gulp.task('compress-scripts', function() {
+	return gulp.src([paths.scripts + '*-ck.js', paths.scripts + 'custom.modernizr.js'])
 		.pipe(closureCompiler({language_in: 'ECMASCRIPT5_STRICT'}))
 		.pipe(gulp.dest(projectRoot+'js'));
 });
 
-gulp.task('images', function() {
+gulp.task('optimize-images', function() {
 	return gulp.src(paths.images + '*.png')
 		.pipe(imagemin({optimizationLevel: 5}))
-		.pipe(gulp.dest(projectRoot+'images'));
+		.pipe(gulp.dest(paths.images));
 });
 
-gulp.task('sass', function() {
+gulp.task('compile-sass', function() {
 	return gulp.src(paths.scss + '*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest(projectRoot+'css'));
+		.pipe(gulp.dest(paths.css));
 });
 
-gulp.task('default', ['scripts','images','sass']);
+gulp.task('compress-css', ['compile-sass'], function() {
+	return gulp.src(paths.css + '*.css')
+		.pipe(minifyCSS())
+		.pipe(gulp.dest(paths.css));
+});
+
+gulp.task('git-status', ['compress-scripts','optimize-images','compile-sass', 'compress-css'], shell.task([
+	'git status'
+]));
+
+gulp.task('default', ['compress-scripts','optimize-images','compile-sass', 'compress-css']);
