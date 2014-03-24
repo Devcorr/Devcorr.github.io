2aa376a87c07e0eaa6afed44fe224909c7ead77e
joeymadich <joe@devcorr.com>
2014-03-24 14:50:48 -0400

    updating gulpfile

--- ansible/gulpfile.js
+++ ansible/gulpfile.js
@@ -40,12 +40,12 @@ gulp.task('compress-css', ['compile-sass'], function() {
 		.pipe(gulp.dest(paths.css));
 });
 
-gulp.task('github-deployment', ['compress-scripts','optimize-images','compile-sass', 'compress-css'], shell.task([
+gulp.task('github-deployment', shell.task([
 	'git status',
 	'git stash',
 	'git checkout master',
 	'git stash pop',
-	'git add -f --all',
+	'git add --all',
 	'now=date',
 	'git commit -m "deployment executed $now"',
 	'git push origin master',
