3fa2ab40b06bfc072f2f9c32a0780ce3cd1a0065
joeymadich <joe@devcorr.com>
2014-03-25 14:26:52 -0400

    update gulpfile

--- ansible/gulpfile.js
+++ ansible/gulpfile.js
@@ -16,19 +16,23 @@ var paths = {
 	images: projectRoot + 'images/'
 };
 
-gulp.task('compress-scripts', function() {
+gulp.task(['update-dev-branch'], shell.task([
+	'git pull'
+]));
+
+gulp.task('compress-scripts', ['update-dev-branch'], function() {
 	return gulp.src([paths.scripts + '*-ck.js', paths.scripts + 'custom.modernizr.js'])
 		.pipe(closureCompiler({language_in: 'ECMASCRIPT5_STRICT'}))
 		.pipe(gulp.dest(projectRoot+'js'));
 });
 
-gulp.task('optimize-images', function() {
+gulp.task('optimize-images', ['update-dev-branch'], function() {
 	return gulp.src(paths.images + '*.png')
 		.pipe(imagemin({optimizationLevel: 5}))
 		.pipe(gulp.dest(paths.images));
 });
 
-gulp.task('compile-sass', function() {
+gulp.task('compile-sass', ['update-dev-branch'], function() {
 	return gulp.src(paths.scss + '*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest(paths.css));
@@ -40,8 +44,19 @@ gulp.task('compress-css', ['compile-sass'], function() {
 		.pipe(gulp.dest(paths.css));
 });
 
-gulp.task('git-status', ['compress-scripts','optimize-images','compile-sass', 'compress-css'], shell.task([
-	'git status'
+gulp.task('deploy-to-github', ['update-dev-branch','compress-scripts','optimize-images','compress-css'], shell.task([
+	'git commit -m "deploying to github pages"',
+	'git push origin dev',
+	'git checkout master',
+	'git reset --hard dev',
+	'cp ' + projectRoot + 'index.html ../index.html && cp ' + projectRoot + 'robots.txt ../robots.txt',
+	"find " + paths.scripts + " -type f -not -name '*-ck.js' -and -not -name 'custom.modernizr.js' | xargs rm",
+	'cp ' + paths.scripts + '../js',
+	'cp ' + paths.css + '../css',
+	'cp ' + paths.fonts + '../fonts',
+	'cp ' + paths.images + '../images',
+	'rm -rf ../ansible ../www',
+	'rm tumblr.html Vagrantfile'
 ]));
 
-gulp.task('default', ['compress-scripts','optimize-images','compile-sass', 'compress-css']);
+gulp.task('default', ['deploy-to-github']);
